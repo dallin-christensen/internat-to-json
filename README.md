582d3840c6cc4d9f0d8f65d12066895fe6e9a170
Dallin Christensen <christensen.dallin@gmail.com>
2020-12-29 16:23:39 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Internationalization to-json tool
+
+for quickly turning our application language phrases into useful JSON files for PK.
+
+### how to use:
+
+- clone repo
+- download google sheets document as csv file and place at the top of this project directory named "translations.csv"
+```
+npm i
+node .
+```
+- copy files in /dist into PK application
+
+
+Note: Currently this code will add asterisks to all strings as to make our lives easier when refactoring the application.
---
+++ index.js
@@ -0,0 +1,91 @@
+const parse = require('csv-parse')
+const fs = require('fs')
+const fsExtra = require('fs-extra')
+
+const languages = {
+  en: {
+    name: 'english',
+    row: 2,
+  },
+  fr: {
+    name: 'french',
+    row: 3,
+  },
+  sp: {
+    name: 'spanish',
+    row: 4,
+  },
+}
+
+const selectedLanguage = 'en'
+
+const languageName = languages[selectedLanguage].name
+const languageRow = languages[selectedLanguage].row
+
+
+const csvData = {};
+const repeatedIds = [];
+let totalIdAmount = 0;
+
+
+const deleteFilesInDist = () => {
+  fsExtra.emptyDirSync(`./dist/${languageName}`)
+}
+
+const writeFiles = () => {
+
+  const sections = Object.keys(csvData)
+
+  sections.forEach(sectionName => {
+    const section = csvData[sectionName]
+    const data = JSON.stringify(section, null, 2)
+
+    fs.writeFileSync(`./dist/${languageName}/${sectionName}.json`, data)
+  })
+
+  if (repeatedIds.length) {
+    console.log({ repeatedIds })
+  }
+
+  console.log(`Contratulations, ${totalIdAmount} ${languageName} id's created!`)
+}
+
+
+const handleParsedData = () => {
+  deleteFilesInDist()
+  writeFiles()
+}
+
+
+const parseCsvData = () =>{
+  fs.createReadStream('./translations.csv')
+  .pipe(parse({delimiter: ','}))
+  .on('data', function(csvrow) {
+    
+    const phraseId = csvrow[6].toLowerCase()
+
+    if (phraseId && csvrow[languageRow] && phraseId !== 'id') {
+
+      const section = phraseId.slice(0, phraseId.indexOf('-'))
+
+      if (!csvData[section]) {
+        csvData[section] = {}
+      }
+
+      if (csvData[section][phraseId]) {
+        repeatedIds.push(phraseId)
+      } else {
+        totalIdAmount++;
+      }
+
+      csvData[section][phraseId] = csvrow[languageRow] + ' **'
+    }
+  })
+  .on('end',function() {
+    handleParsedData()
+  });
+}
+
+const execute = () => parseCsvData()
+
+execute()
